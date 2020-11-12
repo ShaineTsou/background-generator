d7f917e61f53127bb2584b988d0d0162d9e2a45f
Shaine Tsou <shaineagro06@gmail.com>
2020-11-12 21:07:37 +0800

    testing browserify

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var _ = require('lodash');
+console.log(_);
+
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer: ', _.without(array, 3));
+
+
 var bodyBg = document.getElementById("bodyBg");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
