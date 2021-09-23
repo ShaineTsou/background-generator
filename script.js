79891d6e52c00982ae4ae0d14bf070a80d12c803
Shaine Tsou <shaineagro06@gmail.com>
2021-09-23 22:23:08 +0800

    added button that randomly pick background color

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const body = document.querySelector("#body");
 const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
+const randomBtn = document.querySelector("#random-btn");
 const bgCodeContainer = document.querySelector("#bg-code-container");
 const bgCode = document.querySelector("#bg-code");
 const copied = document.querySelector("#copied");
@@ -24,6 +25,25 @@ const changeBackground = function () {
   }
 };
 
+// Pick random background color
+const getRandomHexColor = function () {
+  const hexColorArr = [];
+
+  for (let i = 0; i < 3; i++) {
+    const randomHex = Math.floor(Math.random() * 255).toString(16);
+    const hexColor = randomHex.length === 1 ? `0${randomHex}` : randomHex;
+    hexColorArr.push(hexColor);
+  }
+
+  return `#${hexColorArr.join("")}`;
+};
+
+const getRandomBackground = function () {
+  color1.value = getRandomHexColor();
+  color2.value = getRandomHexColor();
+  changeBackground();
+};
+
 async function clipboardCopy() {
   let text = bgCode.textContent;
   await navigator.clipboard.writeText(text);
@@ -35,3 +55,4 @@ async function clipboardCopy() {
 color1.addEventListener("input", changeBackground);
 color2.addEventListener("input", changeBackground);
 bgCode.addEventListener("click", clipboardCopy);
+randomBtn.addEventListener("click", getRandomBackground);
