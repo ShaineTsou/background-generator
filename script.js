a957aaafe51a119fbac9d3eaa22027c2d7da0b55
Shaine Tsou <shaineagro06@gmail.com>
2021-09-23 23:11:48 +0800

    added direction select

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const body = document.querySelector("#body");
 const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
+const direction = document.querySelector("#direction");
 const randomBtn = document.querySelector("#random-btn");
 const bgCodeContainer = document.querySelector("#bg-code-container");
 const bgCode = document.querySelector("#bg-code");
 const copied = document.querySelector("#copied");
 
 // Set initial background
-body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+body.style.background = `linear-gradient(to ${direction.value}, ${color1.value}, ${color2.value})`;
 
 const changeBackground = function () {
   // Display bgCodeContainer
@@ -16,7 +17,7 @@ const changeBackground = function () {
   }
 
   bgCodeContainer.style.display = "flex";
-  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  body.style.background = `linear-gradient(to ${direction.value}, ${color1.value}, ${color2.value})`;
   bgCode.textContent = `background: ${body.style.background};`;
 
   // Hide copied
@@ -25,12 +26,19 @@ const changeBackground = function () {
   }
 };
 
-// Pick random background color
+// Pick random background
+const getRandomBgDirection = function () {
+  const directions = ["top", "right", "bottom", "left"];
+  const randomIndex = Math.floor(Math.random() * 4);
+
+  return directions[randomIndex];
+};
+
 const getRandomHexColor = function () {
   const hexColorArr = [];
 
   for (let i = 0; i < 3; i++) {
-    const randomHex = Math.floor(Math.random() * 255).toString(16);
+    const randomHex = Math.floor(Math.random() * 256).toString(16);
     const hexColor = randomHex.length === 1 ? `0${randomHex}` : randomHex;
     hexColorArr.push(hexColor);
   }
@@ -41,6 +49,7 @@ const getRandomHexColor = function () {
 const getRandomBackground = function () {
   color1.value = getRandomHexColor();
   color2.value = getRandomHexColor();
+  direction.value = getRandomBgDirection();
   changeBackground();
 };
 
@@ -54,5 +63,6 @@ async function clipboardCopy() {
 
 color1.addEventListener("input", changeBackground);
 color2.addEventListener("input", changeBackground);
+direction.addEventListener("input", changeBackground);
 bgCode.addEventListener("click", clipboardCopy);
 randomBtn.addEventListener("click", getRandomBackground);
